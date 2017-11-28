c51d3219e651ef048eaeb5242aa713ad03183117
Zhenya <zhenya@meteor.com>
2017-11-27 16:01:45 -0800

    swapped for markdown-it to fix tables

--- buildMd.js
+++ buildMd.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const fs = require('fs');
-const showdown = require('showdown');
-const converter = new showdown.Converter();
+
 const mdpath = path.resolve(__dirname, 'src/content.md');
 const outpath = path.resolve(__dirname, 'src/templates/content.html');
 const md = fs.readFileSync(mdpath).toString();
-const html = converter.makeHtml(md) + '\n';
+
+// const showdown = require('showdown');
+// const converter = new showdown.Converter();
+// const html = converter.makeHtml(md) + '\n';
+
+// const md2html = require('github-markdown');
+// const html = md2html(md);
+
+const Markdown = require('markdown-it');
+const convert = new Markdown();
+const html = convert.render(md);
+
 fs.writeFileSync(outpath, html);
 console.log('built content.html');
